94c85764db97c484398c211a5c50b1cf1cad7f16
Danilo Cabello <daniloc@nationalfibre.net>
2014-04-04 20:16:41 +0000

    ARCH-564 Protect RPC layer by using Model, Validation and Filter.

--- DependencyInjection/ICBaseTranslatorExtension.php
+++ DependencyInjection/ICBaseTranslatorExtension.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 class ICBaseTranslatorExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
